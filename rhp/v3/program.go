786c8e9370e4ba3179eab434ac7630c8c6416b1a
lukechampine <luke.champine@gmail.com>
2023-02-03 01:01:59 -0500

    rhp: Add packages

---
+++ rhp/v3/program.go
@@ -0,0 +1,444 @@
+package rhp
+
+import (
+	"go.sia.tech/core/types"
+)
+
+type (
+	// An Instruction is an MDM instruction.
+	Instruction interface {
+		ProtocolObject
+		RequiresContract() bool
+		RequiresFinalization() bool
+	}
+
+	// InstrAppendSector stores a sector on the host and appends its Merkle root
+	// to a contract.
+	InstrAppendSector struct {
+		SectorDataOffset uint64
+		ProofRequired    bool
+	}
+
+	// InstrAppendSectorRoot appends a sector root to a contract
+	InstrAppendSectorRoot struct {
+		MerkleRootOffset uint64
+		ProofRequired    bool
+	}
+
+	// InstrDropSectors removes the last n sectors from a contract and deletes
+	// them from the host.
+	InstrDropSectors struct {
+		SectorCountOffset uint64
+		ProofRequired     bool
+	}
+
+	// InstrHasSector checks if a sector is present on the host.
+	InstrHasSector struct {
+		MerkleRootOffset uint64
+	}
+
+	// InstrReadOffset reads a range of bytes from an offset in the contract.
+	InstrReadOffset struct {
+		LengthOffset  uint64
+		OffsetOffset  uint64
+		ProofRequired bool
+	}
+
+	// InstrReadSector reads a range of bytes from a sector.
+	InstrReadSector struct {
+		LengthOffset     uint64
+		OffsetOffset     uint64
+		MerkleRootOffset uint64
+		ProofRequired    bool
+	}
+
+	// InstrSwapSector swaps two sectors in a contract
+	InstrSwapSector struct {
+		Sector1Offset uint64
+		Sector2Offset uint64
+		ProofRequired bool
+	}
+
+	// InstrUpdateSector overwrites data in an existing sector.
+	InstrUpdateSector struct {
+		Offset        uint64
+		Length        uint64
+		DataOffset    uint64
+		ProofRequired bool
+	}
+
+	// InstrStoreSector temporarily stores a sector on the host. The sector is
+	// not associated with any contract and collateral is not risked.
+	InstrStoreSector struct {
+		DataOffset uint64
+		Duration   uint64
+	}
+
+	// InstrRevision returns the latest revision of a contract
+	InstrRevision struct{}
+
+	// InstrReadRegistry reads a value from the registry
+	InstrReadRegistry struct {
+		PublicKeyOffset uint64
+		PublicKeyLength uint64
+		TweakOffset     uint64
+		Version         uint8
+	}
+
+	// InstrReadRegistryNoVersion reads a pre-1.5.7 read registry Instruction
+	// without the version byte
+	InstrReadRegistryNoVersion struct {
+		InstrReadRegistry
+	}
+
+	// InstrUpdateRegistry updates a registry value.
+	InstrUpdateRegistry struct {
+		TweakOffset     uint64
+		RevisionOffset  uint64
+		SignatureOffset uint64
+		PublicKeyOffset uint64
+		PublicKeyLength uint64
+		DataOffset      uint64
+		DataLength      uint64
+		EntryType       uint8
+	}
+
+	// InstrUpdateRegistryNoType reads a pre-1.5.7 update registry Instruction
+	// without the entry type byte
+	InstrUpdateRegistryNoType struct {
+		InstrUpdateRegistry
+	}
+)
+
+var (
+	idInstrAppendSector     = types.NewSpecifier("Append")
+	idInstrAppendSectorRoot = types.NewSpecifier("AppendSectorRoot")
+	idInstrDropSectors      = types.NewSpecifier("DropSectors")
+	idInstrHasSector        = types.NewSpecifier("HasSector")
+	idInstrStoreSector      = types.NewSpecifier("StoreSector")
+	idInstrUpdateSector     = types.NewSpecifier("UpdateSector")
+	idInstrReadOffset       = types.NewSpecifier("ReadOffset")
+	idInstrReadSector       = types.NewSpecifier("ReadSector")
+	idInstrContractRevision = types.NewSpecifier("Revision")
+	idInstrSectorRoots      = types.NewSpecifier("SectorRoots")
+	idInstrSwapSector       = types.NewSpecifier("SwapSector")
+	idInstrUpdateRegistry   = types.NewSpecifier("UpdateRegistry")
+	idInstrReadRegistry     = types.NewSpecifier("ReadRegistry")
+	idInstrReadRegistrySID  = types.NewSpecifier("ReadRegistrySID")
+)
+
+// RequiresContract implements Instruction.
+func (i *InstrAppendSector) RequiresContract() bool {
+	return true
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrAppendSector) RequiresFinalization() bool {
+	return true
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrAppendSector) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.SectorDataOffset)
+	e.WriteBool(i.ProofRequired)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrAppendSector) DecodeFrom(d *types.Decoder) {
+	i.SectorDataOffset = d.ReadUint64()
+	i.ProofRequired = d.ReadBool()
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrAppendSectorRoot) RequiresContract() bool {
+	return true
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrAppendSectorRoot) RequiresFinalization() bool {
+	return true
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrAppendSectorRoot) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.MerkleRootOffset)
+	e.WriteBool(i.ProofRequired)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrAppendSectorRoot) DecodeFrom(d *types.Decoder) {
+	i.MerkleRootOffset = d.ReadUint64()
+	i.ProofRequired = d.ReadBool()
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrDropSectors) RequiresContract() bool {
+	return true
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrDropSectors) RequiresFinalization() bool {
+	return true
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrDropSectors) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.SectorCountOffset)
+	e.WriteBool(i.ProofRequired)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrDropSectors) DecodeFrom(d *types.Decoder) {
+	i.SectorCountOffset = d.ReadUint64()
+	i.ProofRequired = d.ReadBool()
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrHasSector) RequiresContract() bool {
+	return false
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrHasSector) RequiresFinalization() bool {
+	return false
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrHasSector) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.MerkleRootOffset)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrHasSector) DecodeFrom(d *types.Decoder) {
+	i.MerkleRootOffset = d.ReadUint64()
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrReadOffset) RequiresContract() bool {
+	return true
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrReadOffset) RequiresFinalization() bool {
+	return false
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrReadOffset) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.LengthOffset)
+	e.WriteUint64(i.OffsetOffset)
+	e.WriteBool(i.ProofRequired)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrReadOffset) DecodeFrom(d *types.Decoder) {
+	i.LengthOffset = d.ReadUint64()
+	i.OffsetOffset = d.ReadUint64()
+	i.ProofRequired = d.ReadBool()
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrReadSector) RequiresContract() bool {
+	return true
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrReadSector) RequiresFinalization() bool {
+	return false
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrReadSector) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.LengthOffset)
+	e.WriteUint64(i.OffsetOffset)
+	e.WriteUint64(i.MerkleRootOffset)
+	e.WriteBool(i.ProofRequired)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrReadSector) DecodeFrom(d *types.Decoder) {
+	i.LengthOffset = d.ReadUint64()
+	i.OffsetOffset = d.ReadUint64()
+	i.MerkleRootOffset = d.ReadUint64()
+	i.ProofRequired = d.ReadBool()
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrSwapSector) RequiresContract() bool {
+	return true
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrSwapSector) RequiresFinalization() bool {
+	return true
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrSwapSector) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.Sector1Offset)
+	e.WriteUint64(i.Sector2Offset)
+	e.WriteBool(i.ProofRequired)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrSwapSector) DecodeFrom(d *types.Decoder) {
+	i.Sector1Offset = d.ReadUint64()
+	i.Sector2Offset = d.ReadUint64()
+	i.ProofRequired = d.ReadBool()
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrUpdateSector) RequiresContract() bool {
+	return true
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrUpdateSector) RequiresFinalization() bool {
+	return true
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrUpdateSector) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.Offset)
+	e.WriteUint64(i.Length)
+	e.WriteUint64(i.DataOffset)
+	e.WriteBool(i.ProofRequired)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrUpdateSector) DecodeFrom(d *types.Decoder) {
+	i.Offset = d.ReadUint64()
+	i.Length = d.ReadUint64()
+	i.DataOffset = d.ReadUint64()
+	i.ProofRequired = d.ReadBool()
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrStoreSector) RequiresContract() bool {
+	return false
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrStoreSector) RequiresFinalization() bool {
+	return false
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrStoreSector) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.DataOffset)
+	e.WriteUint64(i.Duration)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrStoreSector) DecodeFrom(d *types.Decoder) {
+	i.DataOffset = d.ReadUint64()
+	i.Duration = d.ReadUint64()
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrRevision) RequiresContract() bool {
+	return true
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrRevision) RequiresFinalization() bool {
+	return false
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrRevision) EncodeTo(e *types.Encoder) {
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrRevision) DecodeFrom(d *types.Decoder) {
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrReadRegistry) RequiresContract() bool {
+	return false
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrReadRegistry) RequiresFinalization() bool {
+	return false
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrReadRegistry) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.PublicKeyOffset)
+	e.WriteUint64(i.PublicKeyLength)
+	e.WriteUint64(i.TweakOffset)
+	e.WriteUint8(i.Version)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrReadRegistry) DecodeFrom(d *types.Decoder) {
+	i.PublicKeyOffset = d.ReadUint64()
+	i.PublicKeyLength = d.ReadUint64()
+	i.TweakOffset = d.ReadUint64()
+	i.Version = d.ReadUint8()
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrReadRegistryNoVersion) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.PublicKeyOffset)
+	e.WriteUint64(i.PublicKeyLength)
+	e.WriteUint64(i.TweakOffset)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrReadRegistryNoVersion) DecodeFrom(d *types.Decoder) {
+	i.PublicKeyOffset = d.ReadUint64()
+	i.PublicKeyLength = d.ReadUint64()
+	i.TweakOffset = d.ReadUint64()
+	i.Version = 1
+}
+
+// RequiresContract implements Instruction.
+func (i *InstrUpdateRegistry) RequiresContract() bool {
+	return false
+}
+
+// RequiresFinalization implements Instruction.
+func (i *InstrUpdateRegistry) RequiresFinalization() bool {
+	return false
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrUpdateRegistry) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.TweakOffset)
+	e.WriteUint64(i.RevisionOffset)
+	e.WriteUint64(i.SignatureOffset)
+	e.WriteUint64(i.PublicKeyOffset)
+	e.WriteUint64(i.PublicKeyLength)
+	e.WriteUint8(uint8(i.EntryType))
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrUpdateRegistry) DecodeFrom(d *types.Decoder) {
+	i.TweakOffset = d.ReadUint64()
+	i.RevisionOffset = d.ReadUint64()
+	i.SignatureOffset = d.ReadUint64()
+	i.PublicKeyOffset = d.ReadUint64()
+	i.PublicKeyLength = d.ReadUint64()
+	i.EntryType = d.ReadUint8()
+}
+
+// EncodeTo implements Instruction.
+func (i *InstrUpdateRegistryNoType) EncodeTo(e *types.Encoder) {
+	e.WriteUint64(i.TweakOffset)
+	e.WriteUint64(i.RevisionOffset)
+	e.WriteUint64(i.SignatureOffset)
+	e.WriteUint64(i.PublicKeyOffset)
+	e.WriteUint64(i.PublicKeyLength)
+}
+
+// DecodeFrom implements Instruction.
+func (i *InstrUpdateRegistryNoType) DecodeFrom(d *types.Decoder) {
+	i.TweakOffset = d.ReadUint64()
+	i.RevisionOffset = d.ReadUint64()
+	i.SignatureOffset = d.ReadUint64()
+	i.PublicKeyOffset = d.ReadUint64()
+	i.PublicKeyLength = d.ReadUint64()
+	i.EntryType = EntryTypeArbitrary
+}
---
+++ rhp/v3/registry.go
@@ -0,0 +1,149 @@
+package rhp
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+
+	"go.sia.tech/core/types"
+)
+
+const (
+	// EntryTypeArbitrary is a registry value where all data is arbitrary.
+	EntryTypeArbitrary = iota + 1
+
+	// EntryTypePubKey is a registry value where the first 20 bytes of data
+	// corresponds to the hash of a host's public key.
+	EntryTypePubKey
+)
+
+const (
+	// MaxValueDataSize is the maximum size of a Value's Data
+	// field.
+	MaxValueDataSize = 113
+)
+
+// A RegistryKey uniquely identifies a value in the host's registry.
+type RegistryKey struct {
+	PublicKey types.PublicKey
+	Tweak     types.Hash256
+}
+
+// A RegistryValue is a value associated with a key and a tweak in a host's
+// registry.
+type RegistryValue struct {
+	Data      []byte
+	Revision  uint64
+	Type      uint8
+	Signature types.Signature
+}
+
+// A RegistryEntry contains the data stored by a host for each registry value.
+type RegistryEntry struct {
+	RegistryKey
+	RegistryValue
+}
+
+// Hash returns the hash of the key.
+func (rk *RegistryKey) Hash() types.Hash256 {
+	h := types.NewHasher()
+	rk.PublicKey.UnlockKey().EncodeTo(h.E)
+	rk.Tweak.EncodeTo(h.E)
+	return h.Sum()
+}
+
+// Hash returns the hash used for signing the entry.
+func (re *RegistryEntry) Hash() types.Hash256 {
+	h := types.NewHasher()
+	re.Tweak.EncodeTo(h.E)
+	h.E.WriteBytes(re.Data)
+	h.E.WriteUint64(re.Revision)
+	if re.Type == EntryTypePubKey {
+		h.E.WriteUint8(re.Type)
+	}
+	return h.Sum()
+}
+
+// Work returns the work of an entry.
+func (re *RegistryEntry) Work() types.Hash256 {
+	data := re.Data
+	if re.Type == EntryTypePubKey {
+		data = re.Data[20:]
+	}
+	h := types.NewHasher()
+	re.Tweak.EncodeTo(h.E)
+	h.E.WriteBytes(data)
+	h.E.WriteUint64(re.Revision)
+	return h.Sum()
+}
+
+// CompareRegistryWork compares the work of two registry entries.
+func CompareRegistryWork(r1, r2 RegistryEntry) int {
+	r1w, r2w := r1.Work(), r2.Work()
+	return bytes.Compare(r1w[:], r2w[:])
+}
+
+// RegistryHostID returns the ID hash of the host for primary registry entries.
+func RegistryHostID(pk types.PublicKey) types.Hash256 {
+	h := types.NewHasher()
+	pk.UnlockKey().EncodeTo(h.E)
+	return h.Sum()
+}
+
+// ValidateRegistryEntry validates the fields of a registry entry.
+func ValidateRegistryEntry(re RegistryEntry) (err error) {
+	switch re.Type {
+	case EntryTypeArbitrary:
+		break // no extra validation required
+	case EntryTypePubKey:
+		// pub key entries have the first 20 bytes of the host's pub key hash
+		// prefixed to the data.
+		if len(re.Data) < 20 {
+			return errors.New("expected host public key hash")
+		}
+	default:
+		return fmt.Errorf("invalid registry value type: %d", re.Type)
+	}
+	if !re.PublicKey.VerifyHash(re.Hash(), re.Signature) {
+		return errors.New("invalid signature")
+	}
+	return nil
+}
+
+// ValidateRegistryUpdate validates a registry update against the current entry.
+// An updated registry entry must have a greater revision number, more work, or
+// be replacing a non-primary registry entry.
+func ValidateRegistryUpdate(old, update RegistryEntry, hostID types.Hash256) error {
+	// if the new revision is greater than the current revision, the update is
+	// valid.
+	if update.Revision > old.Revision {
+		return nil
+	} else if update.Revision < old.Revision {
+		return errors.New("update revision must be greater than current revision")
+	}
+
+	// if the revision number is the same, but the work is greater, the update
+	// is valid.
+	if w := CompareRegistryWork(update, old); w > 0 {
+		return nil
+	} else if w < 0 {
+		return errors.New("update must have greater work or greater revision number than current entry")
+	}
+
+	// if the update entry is an arbitrary value entry, the update is invalid.
+	if update.Type == EntryTypeArbitrary {
+		return errors.New("update must be a primary entry or have a greater revision number")
+	}
+
+	// if the updated entry is not a primary entry, it is invalid.
+	if !bytes.Equal(update.Data[:20], hostID[:20]) {
+		return errors.New("update must be a primary entry or have a greater revision number")
+	}
+
+	// if the update and current entry are both primary, the update is invalid
+	if old.Type == EntryTypePubKey && bytes.Equal(old.Data[:20], hostID[:20]) {
+		return errors.New("update revision must be greater than current revision")
+	}
+
+	return nil
+}
