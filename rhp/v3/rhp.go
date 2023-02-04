11b68568ab7ce2968fd80736ad61b75c827e1c12
Nate Maninger <me@n8m.us>
2023-02-04 08:30:17 -0700

    rhp/v3: add total to ResourceCost

--- rhp/v3/rhp.go
+++ rhp/v3/rhp.go
@@ -240,6 +240,13 @@ func (a *ResourceCost) Add(b ResourceCost) ResourceCost {
 	}
 }
 
+// Total returns the total cost and collateral of a ResourceCost.
+func (a *ResourceCost) Total() (cost, collateral types.Currency) {
+	cost = a.Base.Add(a.Storage).Add(a.Egress).Add(a.Ingress)
+	collateral = a.Collateral
+	return
+}
+
 // writeBaseCost is the cost of executing a 'Write' instruction of a certain length
 // on the MDM.
 func (pt *HostPriceTable) writeBaseCost(writeLength uint64) types.Currency {
