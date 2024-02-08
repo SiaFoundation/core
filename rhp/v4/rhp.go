ffbccc65e6b31da0b562ea70e7be083751d42a9e
lukechampine <luke.champine@gmail.com>
2024-02-08 15:07:03 -0500

    rhp/v4: Add various RPC helpers

--- rhp/v4/rhp.go
+++ rhp/v4/rhp.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"time"
 
+	"go.sia.tech/core/consensus"
 	"go.sia.tech/core/types"
 )
 
@@ -16,6 +17,10 @@ const (
 	// SegmentSize is the size of one segment in bytes.
 	SegmentSize = 64
 )
+
+func round4KiB(n uint64) uint64 {
+	return (n + (1<<12 - 1)) &^ (1<<12 - 1)
+}
 
 // A NetAddress is a pair of protocol and address that a host may be reached on.
 type NetAddress struct {
@@ -36,6 +41,14 @@ type HostPrices struct {
 
 	// covers above fields
 	Signature types.Signature
+}
+
+func (hp HostPrices) ReadSectorCost(length uint64) types.Currency {
+	return hp.EgressPrice.Mul64(round4KiB(length))
+}
+
+func (hp HostPrices) WriteSectorCost(sector []byte) types.Currency {
+	return hp.IngressPrice.Mul64(round4KiB(uint64(len(sector))))
 }
 
 // HostSettings specify the settings of a host.
@@ -164,7 +177,7 @@ type (
 	// RPCRenewContractRequest implements Request.
 	RPCRenewContractRequest struct {
 		Prices        HostPrices
-		Renewal       types.V2FileContractRenewal
+		Renewal       types.V2FileContractResolution
 		RenterInputs  []types.V2SiacoinInput
 		RenterParents []types.V2Transaction
 	}
@@ -328,7 +341,169 @@ var idMap = map[types.Specifier]func() Request{
 	(RPCWriteSectorRequest{}).ID():    func() Request { return new(RPCWriteSectorRequest) },
 }
 
-// RequestforID returns the intial request object for a given ID.
+// RequestforID returns the intial request object for a given ID, or nil if the
+// ID is not recognized.
 func RequestforID(id types.Specifier) Request {
-	return idMap[id]()
+	if r := idMap[id]; r != nil {
+		return r()
+	}
+	return nil
+}
+
+func NewContract(hs HostSettings, allowance, collateral types.Currency, proofHeight uint64, renterAddress types.Address, renterPublicKey, hostPublicKey types.PublicKey) types.V2FileContract {
+	const proofWindow = 144
+	return types.V2FileContract{
+		Filesize:         0,
+		FileMerkleRoot:   types.Hash256{},
+		ProofHeight:      proofHeight,
+		ExpirationHeight: proofHeight + proofWindow,
+		RenterOutput: types.SiacoinOutput{
+			Value:   allowance,
+			Address: renterAddress,
+		},
+		HostOutput: types.SiacoinOutput{
+			Value:   collateral.Add(hs.Prices.ContractPrice),
+			Address: hs.WalletAddress,
+		},
+		HostCollateral:  collateral,
+		RenterPublicKey: renterPublicKey,
+		HostPublicKey:   hostPublicKey,
+		RevisionNumber:  0,
+	}
+}
+
+func ContractCost(cs consensus.State, hs HostSettings, fc types.V2FileContract, minerFee types.Currency) (renter, host types.Currency) {
+	return fc.RenterOutput.Value.Add(hs.Prices.ContractPrice).Add(minerFee), fc.HostOutput.Value.Sub(hs.Prices.ContractPrice)
+}
+
+func FormContractTransaction(cs consensus.State, req RPCFormContractRequest, resp RPCFormContractResponse) types.V2Transaction {
+	txn := types.V2Transaction{
+		FileContracts: []types.V2FileContract{req.Contract},
+		MinerFee:      types.ZeroCurrency, // TODO
+	}
+	var renterSum types.Currency
+	for _, sci := range req.RenterInputs {
+		txn.SiacoinInputs = append(txn.SiacoinInputs, sci)
+		renterSum = renterSum.Add(sci.Parent.SiacoinOutput.Value)
+	}
+	if change := renterSum.Sub(req.Contract.RenterOutput.Value); !change.IsZero() { // TODO
+		txn.SiacoinOutputs = append(txn.SiacoinOutputs, types.SiacoinOutput{
+			Value:   change,
+			Address: req.Contract.RenterOutput.Address,
+		})
+	}
+	var hostSum types.Currency
+	for _, sci := range resp.HostInputs {
+		txn.SiacoinInputs = append(txn.SiacoinInputs, sci)
+		hostSum = hostSum.Add(sci.Parent.SiacoinOutput.Value)
+	}
+	if change := hostSum.Sub(req.Contract.HostOutput.Value); !change.IsZero() { // TODO
+		txn.SiacoinOutputs = append(txn.SiacoinOutputs, types.SiacoinOutput{
+			Value:   change,
+			Address: req.Contract.HostOutput.Address,
+		})
+	}
+	return txn
+}
+
+func SignFormContractTransaction(txn *types.V2Transaction, resp2 RPCFormContractSecondResponse, resp3 RPCFormContractThirdResponse) {
+	txn.FileContracts[0].RenterSignature = resp2.RenterContractSignature
+	txn.FileContracts[0].HostSignature = resp3.HostContractSignature
+	for i := range txn.SiacoinInputs {
+		sci := &txn.SiacoinInputs[i]
+		if i < len(resp2.RenterSatisfiedPolicies) {
+			sci.SatisfiedPolicy = resp2.RenterSatisfiedPolicies[i]
+		} else {
+			sci.SatisfiedPolicy = resp3.HostSatisfiedPolicies[i-len(resp2.RenterSatisfiedPolicies)]
+		}
+	}
+}
+
+func RenewContractTransaction(cs consensus.State, req RPCRenewContractRequest, resp RPCRenewContractResponse) types.V2Transaction {
+	renewal := req.Renewal.Resolution.(*types.V2FileContractRenewal)
+	txn := types.V2Transaction{
+		FileContractResolutions: []types.V2FileContractResolution{req.Renewal},
+		MinerFee:                types.ZeroCurrency, // TODO
+	}
+	var renterSum types.Currency
+	for _, sci := range req.RenterInputs {
+		txn.SiacoinInputs = append(txn.SiacoinInputs, sci)
+		renterSum = renterSum.Add(sci.Parent.SiacoinOutput.Value)
+	}
+	if change := renterSum.Sub(renewal.NewContract.RenterOutput.Value); !change.IsZero() { // TODO
+		txn.SiacoinOutputs = append(txn.SiacoinOutputs, types.SiacoinOutput{
+			Value:   change,
+			Address: renewal.NewContract.RenterOutput.Address,
+		})
+	}
+	var hostSum types.Currency
+	for _, sci := range resp.HostInputs {
+		txn.SiacoinInputs = append(txn.SiacoinInputs, sci)
+		hostSum = hostSum.Add(sci.Parent.SiacoinOutput.Value)
+	}
+	if change := hostSum.Sub(renewal.NewContract.HostOutput.Value); !change.IsZero() { // TODO
+		txn.SiacoinOutputs = append(txn.SiacoinOutputs, types.SiacoinOutput{
+			Value:   change,
+			Address: renewal.NewContract.HostOutput.Address,
+		})
+	}
+	return txn
+}
+
+func SignRenewContractTransaction(txn *types.V2Transaction, resp2 RPCRenewContractSecondResponse, resp3 RPCRenewContractThirdResponse) {
+	txn.FileContracts[0].RenterSignature = resp2.RenterContractSignature
+	txn.FileContracts[0].HostSignature = resp3.HostContractSignature
+	for i := range txn.SiacoinInputs {
+		sci := &txn.SiacoinInputs[i]
+		if i < len(resp2.RenterSatisfiedPolicies) {
+			sci.SatisfiedPolicy = resp2.RenterSatisfiedPolicies[i]
+		} else {
+			sci.SatisfiedPolicy = resp3.HostSatisfiedPolicies[i-len(resp2.RenterSatisfiedPolicies)]
+		}
+	}
+}
+
+func pay(fc types.V2FileContract, amount types.Currency) types.V2FileContract {
+	if fc.RenterOutput.Value.Cmp(amount) < 0 {
+		amount = fc.RenterOutput.Value
+	}
+	fc.RenterOutput.Value = fc.RenterOutput.Value.Sub(amount)
+	fc.HostOutput.Value = fc.HostOutput.Value.Add(amount)
+	return fc
+}
+
+func pledge(fc types.V2FileContract, amount types.Currency) types.V2FileContract {
+	if fc.HostCollateral.Cmp(amount) < 0 {
+		amount = fc.HostCollateral
+	}
+	fc.HostCollateral = fc.HostCollateral.Sub(amount)
+	return fc
+}
+
+func ReviseForModifySectors(fc types.V2FileContract, req RPCModifySectorsRequest, resp RPCModifySectorsResponse) types.V2FileContract {
+	old := fc.Filesize
+	for _, action := range req.Actions {
+		switch action.Type {
+		case ActionAppend:
+			fc.Filesize += SectorSize // NOTE: ingress cost paid via account
+		case ActionTrim:
+			fc.Filesize -= SectorSize * action.N
+		}
+	}
+	if fc.Filesize > old {
+		size := fc.Filesize - old
+		duration := fc.ProofHeight - req.Prices.TipHeight
+		fc = pay(fc, req.Prices.StoragePrice.Mul64(size).Mul64(duration))
+		fc = pledge(fc, req.Prices.Collateral.Mul64(size).Mul64(duration))
+	}
+	fc.FileMerkleRoot = resp.Proof[len(resp.Proof)-1] // TODO
+	return fc
+}
+
+func ReviseForSectorRoots(fc types.V2FileContract, prices HostPrices, numRoots uint64) types.V2FileContract {
+	return pay(fc, prices.EgressPrice.Mul64(round4KiB(32*numRoots)))
+}
+
+func ReviseForFundAccount(fc types.V2FileContract, amount types.Currency) types.V2FileContract {
+	return pay(fc, amount)
 }
