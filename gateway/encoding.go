03b4a06742e87146d6382664b74d1355dd84d641
Nate Maninger <me@n8m.us>
2024-06-04 13:50:58 -0700

    gateway: always allocate

--- gateway/encoding.go
+++ gateway/encoding.go
@@ -247,11 +247,7 @@ func (r *RPCSendBlocks) encodeResponse(e *types.Encoder) {
 	}
 }
 func (r *RPCSendBlocks) decodeResponse(d *types.Decoder) {
-	if n := d.ReadPrefix(); n <= cap(r.Blocks) {
-		r.Blocks = r.Blocks[:n]
-	} else {
-		r.Blocks = make([]types.Block, n)
-	}
+	r.Blocks = make([]types.Block, d.ReadPrefix())
 	for i := range r.Blocks {
 		(*types.V1Block)(&r.Blocks[i]).DecodeFrom(d)
 	}
