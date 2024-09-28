0e77790bd8bfbef4285e9ed1f2e5e565992f09a8
Nate Maninger <me@n8m.us>
2024-09-28 16:28:06 -0400

    rhp4: validate prices in validate functions

--- rhp/v4/validation.go
+++ rhp/v4/validation.go
@@ -8,7 +8,12 @@ import (
 )
 
 // Validate validates a read sector request.
-func (req *RPCReadSectorRequest) Validate() error {
+func (req *RPCReadSectorRequest) Validate(pk types.PublicKey) error {
+	if err := req.Prices.Validate(pk); err != nil {
+		return fmt.Errorf("prices are invalid: %w", err)
+	} else if err := req.Token.Validate(); err != nil {
+		return fmt.Errorf("token is invalid: %w", err)
+	}
 	switch {
 	case req.Length == 0:
 		return errors.New("length must be greater than 0")
@@ -21,20 +26,12 @@ func (req *RPCReadSectorRequest) Validate() error {
 }
 
 // Validate validates a write sector request.
-func (req *RPCWriteSectorRequest) Validate() error {
-	switch {
-	case req.Duration == 0:
-		return errors.New("duration must be greater than 0")
-	case len(req.Sector) == 0:
-		return errors.New("sector must not be empty")
-	case len(req.Sector)%LeafSize != 0:
-		return errors.New("sector must be segment aligned")
+func (req *RPCWriteSectorStreamingRequest) Validate(pk types.PublicKey, maxDuration uint64) error {
+	if err := req.Prices.Validate(pk); err != nil {
+		return fmt.Errorf("prices are invalid: %w", err)
+	} else if err := req.Token.Validate(); err != nil {
+		return fmt.Errorf("token is invalid: %w", err)
 	}
-	return nil
-}
-
-// Validate validates a write sector request.
-func (req *RPCWriteSectorStreamingRequest) Validate() error {
 	switch {
 	case req.Duration == 0:
 		return errors.New("duration must be greater than 0")
@@ -42,14 +39,29 @@ func (req *RPCWriteSectorStreamingRequest) Validate() error {
 		return errors.New("sector must not be empty")
 	case req.DataLength%LeafSize != 0:
 		return errors.New("sector must be segment aligned")
+	case req.DataLength > SectorSize:
+		return errors.New("sector exceeds sector bounds")
+	case req.Duration > maxDuration:
+		return fmt.Errorf("duration exceeds maximum: %d > %d", req.Duration, maxDuration)
 	}
 	return nil
 }
 
-// Validate validates a sector roots request. Signatures are not validated.
-func (req *RPCSectorRootsRequest) Validate(fc types.V2FileContract) error {
-	contractSectors := fc.Filesize / SectorSize
+// Validate validates a modify sectors request. Signatures are not validated.
+func (req *RPCModifySectorsRequest) Validate(pk types.PublicKey, maxActions uint64) error {
+	if err := req.Prices.Validate(pk); err != nil {
+		return fmt.Errorf("prices are invalid: %w", err)
+	}
+	return ValidateModifyActions(req.Actions, maxActions)
+}
 
+// Validate validates a sector roots request. Signatures are not validated.
+func (req *RPCSectorRootsRequest) Validate(pk types.PublicKey, fc types.V2FileContract) error {
+	if err := req.Prices.Validate(pk); err != nil {
+		return fmt.Errorf("prices are invalid: %w", err)
+	}
+
+	contractSectors := fc.Filesize / SectorSize
 	switch {
 	case req.Length == 0:
 		return errors.New("length must be greater than 0")
@@ -60,7 +72,11 @@ func (req *RPCSectorRootsRequest) Validate(fc types.V2FileContract) error {
 }
 
 // Validate validates a form contract request. Prices are not validated
-func (req *RPCFormContractRequest) Validate(hs HostSettings, tip types.ChainIndex) error {
+func (req *RPCFormContractRequest) Validate(pk types.PublicKey, tip types.ChainIndex, maxCollateral types.Currency, maxDuration uint64) error {
+	if err := req.Prices.Validate(pk); err != nil {
+		return fmt.Errorf("prices are invalid: %w", err)
+	}
+
 	// validate the request fields
 	switch {
 	case req.MinerFee.IsZero():
@@ -72,7 +88,7 @@ func (req *RPCFormContractRequest) Validate(hs HostSettings, tip types.ChainInde
 	}
 
 	// validate the contract fields
-	hp := hs.Prices
+	hp := req.Prices
 	expirationHeight := req.Contract.ProofHeight + proofWindow
 	duration := expirationHeight - hp.TipHeight
 	// calculate the minimum allowance required for the contract based on the
@@ -84,10 +100,10 @@ func (req *RPCFormContractRequest) Validate(hs HostSettings, tip types.ChainInde
 		return errors.New("contract expiration height is in the past")
 	case req.Contract.Allowance.IsZero():
 		return errors.New("allowance is zero")
-	case req.Contract.Collateral.Cmp(hs.MaxCollateral) > 0:
-		return fmt.Errorf("collateral %v exceeds max collateral %v", req.Contract.Collateral, hs.MaxCollateral)
-	case duration > hs.MaxContractDuration:
-		return fmt.Errorf("contract duration %v exceeds max duration %v", duration, hs.MaxContractDuration)
+	case req.Contract.Collateral.Cmp(maxCollateral) > 0:
+		return fmt.Errorf("collateral %v exceeds max collateral %v", req.Contract.Collateral, maxCollateral)
+	case duration > maxDuration:
+		return fmt.Errorf("contract duration %v exceeds max duration %v", duration, maxDuration)
 	case req.Contract.Allowance.Cmp(minRenterAllowance) < 0:
 		return fmt.Errorf("allowance %v is less than minimum %v for collateral", req.Contract.Allowance, minRenterAllowance)
 	default:
@@ -96,7 +112,11 @@ func (req *RPCFormContractRequest) Validate(hs HostSettings, tip types.ChainInde
 }
 
 // Validate validates a renew contract request. Prices are not validated
-func (req *RPCRenewContractRequest) Validate(hs HostSettings, tip types.ChainIndex) error {
+func (req *RPCRenewContractRequest) Validate(pk types.PublicKey, tip types.ChainIndex, maxCollateral types.Currency, maxDuration uint64) error {
+	if err := req.Prices.Validate(pk); err != nil {
+		return fmt.Errorf("prices are invalid: %w", err)
+	}
+
 	// validate the request fields
 	switch {
 	case req.MinerFee.IsZero():
@@ -108,7 +128,7 @@ func (req *RPCRenewContractRequest) Validate(hs HostSettings, tip types.ChainInd
 	}
 
 	// validate the contract fields
-	hp := hs.Prices
+	hp := req.Prices
 	expirationHeight := req.Renewal.ProofHeight + proofWindow
 	duration := expirationHeight - hp.TipHeight
 	// calculate the minimum allowance required for the contract based on the
@@ -120,10 +140,10 @@ func (req *RPCRenewContractRequest) Validate(hs HostSettings, tip types.ChainInd
 		return errors.New("contract expiration height is in the past")
 	case req.Renewal.Allowance.IsZero():
 		return errors.New("allowance is zero")
-	case req.Renewal.Collateral.Cmp(hs.MaxCollateral) > 0:
-		return fmt.Errorf("collateral %v exceeds max collateral %v", req.Renewal.Collateral, hs.MaxCollateral)
-	case duration > hs.MaxContractDuration:
-		return fmt.Errorf("contract duration %v exceeds max duration %v", duration, hs.MaxContractDuration)
+	case req.Renewal.Collateral.Cmp(maxCollateral) > 0:
+		return fmt.Errorf("collateral %v exceeds max collateral %v", req.Renewal.Collateral, maxCollateral)
+	case duration > maxDuration:
+		return fmt.Errorf("contract duration %v exceeds max duration %v", duration, maxDuration)
 	case req.Renewal.Allowance.Cmp(minRenterAllowance) < 0:
 		return fmt.Errorf("allowance %v is less than minimum %v for collateral", req.Renewal.Allowance, minRenterAllowance)
 	default:
@@ -132,8 +152,12 @@ func (req *RPCRenewContractRequest) Validate(hs HostSettings, tip types.ChainInd
 }
 
 // Validate checks that the request is valid
-func (req *RPCVerifySectorRequest) Validate() error {
-	if req.LeafIndex >= LeavesPerSector {
+func (req *RPCVerifySectorRequest) Validate(pk types.PublicKey) error {
+	if err := req.Prices.Validate(pk); err != nil {
+		return fmt.Errorf("prices are invalid: %w", err)
+	} else if err := req.Token.Validate(); err != nil {
+		return fmt.Errorf("token is invalid: %w", err)
+	} else if req.LeafIndex >= LeavesPerSector {
 		return fmt.Errorf("leaf index must be less than %d", LeavesPerSector)
 	}
 	return nil
