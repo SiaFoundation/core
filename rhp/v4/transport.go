f6a828f191af7c4c99a2dc73b3afae59eff60c3e
Nate Maninger <me@n8m.us>
2024-10-24 11:10:12 -0700

    rhp4: remove debugging

--- rhp/v4/transport.go
+++ rhp/v4/transport.go
@@ -1,8 +1,6 @@
 package rhp
 
 import (
-	"bytes"
-	"fmt"
 	"io"
 	"log"
 
@@ -34,11 +32,9 @@ func ReadID(r io.Reader) (id types.Specifier, err error) {
 func WriteRequest(w io.Writer, id types.Specifier, o Object) error {
 	return withEncoder(w, func(e *types.Encoder) {
 		id.EncodeTo(e)
-		fmt.Println("wrote request id")
 		if o == nil {
 			return
 		}
-		fmt.Printf("writing request %T\n", o)
 		o.encodeTo(e)
 	})
 }
@@ -47,40 +43,28 @@ func WriteRequest(w io.Writer, id types.Specifier, o Object) error {
 func ReadRequest(r io.Reader, o Object) error {
 	return withDecoder(r, o.maxLen(), func(d *types.Decoder) {
 		o.decodeFrom(d)
-		fmt.Printf("read request %T\n", o)
 	})
 }
 
 // WriteResponse writes a response to the stream. Note that RPCError implements
 // Object, and may be used as a response to any RPC.
 func WriteResponse(w io.Writer, o Object) error {
-	buf := bytes.NewBuffer(nil)
-	w = io.MultiWriter(w, buf)
-	err := withEncoder(w, func(e *types.Encoder) {
+	return withEncoder(w, func(e *types.Encoder) {
 		_, isErr := o.(*RPCError)
 		e.WriteBool(isErr)
 		o.encodeTo(e)
-		fmt.Printf("wrote response %T\n", o)
 	})
-	fmt.Printf("write response %T: %v\n", o, buf.Bytes())
-	return err
 }
 
 // ReadResponse reads a response from the stream into r.
 func ReadResponse(r io.Reader, o Object) error {
-	buf := bytes.NewBuffer(nil)
-	r = io.TeeReader(r, buf)
-	err := withDecoder(r, (*RPCError)(nil).maxLen()+o.maxLen(), func(d *types.Decoder) {
+	return withDecoder(r, (*RPCError)(nil).maxLen()+o.maxLen(), func(d *types.Decoder) {
 		if d.ReadBool() {
 			r := new(RPCError)
 			r.decodeFrom(d)
 			d.SetErr(r)
-			fmt.Printf("read response error\n")
 			return
 		}
 		o.decodeFrom(d)
-		fmt.Printf("read response %T\n", o)
 	})
-	fmt.Printf("read response %T: %v\n", o, buf.Bytes())
-	return err
 }
