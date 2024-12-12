8383d656cbed9a0aaa0e09edf1751e44043c2b31
Nate <me@n8m.us>
2024-12-12 11:13:04 -0800

    improve docstring

--- rhp/v4/rhp.go
+++ rhp/v4/rhp.go
@@ -382,6 +382,12 @@ type (
 		ContractID types.FileContractID `json:"contractID"`
 	}
 	// RPCLatestRevisionResponse implements Object.
+	//
+	// The `Revisable` field indicates whether the
+	// host will accept further revisions to the contract. A host will not accept revisions too
+	// close to the proof window or revisions on contracts that have already been resolved.
+	// The `Renewed` field indicates whether the contract was renewed. If the contract was
+	// renewed, the renter can use `FileContractID.V2RenewalID` to get the ID of the new contract.
 	RPCLatestRevisionResponse struct {
 		Contract  types.V2FileContract `json:"contract"`
 		Revisable bool                 `json:"revisable"`
